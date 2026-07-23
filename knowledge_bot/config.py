ea784c6b3a614a7668b424b634c7b7872e0f40ab
ainazeaze <aratefiarivony8@gmail.com>
2026-07-23 08:45:15 +0200

    feat : add reranker config

--- knowledge_bot/config.py
+++ knowledge_bot/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "paraphrase-multilingual-MiniLM-L12-v2")
+RERANKER_MODEL = os.getenv("RERANKER_MODEL", "cross-encoder/ms-marco-MiniLM-L-6-v2")
 CHROMA_DB_PATH = os.getenv("CHROMA_DB_PATH", "./chroma_data")
 TOP_K = int(os.getenv("TOP_K", "5"))
 CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", "2000"))
