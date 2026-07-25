18dc508af3c94a684239a75a9098d3211fabdfab
ainazeaze <aratefiarivony8@gmail.com>
2026-07-25 20:21:27 +0200

    build : dependencies for agentic retrieval

--- knowledge_bot/config.py
+++ knowledge_bot/config.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
+GROQ_MODEL = os.getenv("GROQ_MODEL", "llama-3.1.-8b-instant")
+
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "paraphrase-multilingual-MiniLM-L12-v2")
 RERANKER_MODEL = os.getenv("RERANKER_MODEL", "cross-encoder/ms-marco-MiniLM-L-6-v2")
 CHROMA_DB_PATH = os.getenv("CHROMA_DB_PATH", "./chroma_data")
