ffe8794c53730a349b12016273866d94b7abea6c
ainazeaze <aratefiarivony8@gmail.com>
2026-07-31 18:18:34 +0200

    feat : search schema

---
+++ api/schemas/search.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class SearchResult(BaseModel):
+    text: str
+    title: str
+    source: str
+    doc_id : str
+    score : float
+
+
+class SearchResponse(BaseModel):
+    query : str
+    results: list[SearchResult]
